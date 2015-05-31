afbd3218b8d117fc7524cde471e1acc80e96097a
Michael Barker <mikeb01@gmail.com>
2015-05-31 20:17:31 +1200

    Reduce size of unit_magnitude field.

--- src/hdr_histogram.h
+++ src/hdr_histogram.h
@@ -18,7 +18,7 @@ struct hdr_histogram
 {
     int64_t lowest_trackable_value;
     int64_t highest_trackable_value;
-    int64_t unit_magnitude;
+    int32_t unit_magnitude;
     int64_t significant_figures;
     int32_t sub_bucket_half_count_magnitude;
     int32_t sub_bucket_half_count;
