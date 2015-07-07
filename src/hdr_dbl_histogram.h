b6ec0b32525c03b8bfc26977e2aa999041a85024
Jeremy Fitzhardinge <jeremy@goop.org>
2015-07-07 14:47:34 -0700

    Constify hdr_dbl API.

--- src/hdr_dbl_histogram.h
+++ src/hdr_dbl_histogram.h
@@ -30,25 +30,25 @@ bool hdr_dbl_record_value(struct hdr_dbl_histogram* h, double value);
 bool hdr_dbl_record_values(struct hdr_dbl_histogram* h, double value, int64_t count);
 bool hdr_dbl_record_corrected_value(struct hdr_dbl_histogram* h, double value, double expected_interval);
 bool hdr_dbl_record_corrected_values(struct hdr_dbl_histogram* h, double value, int64_t count, double expected_interval);
-double hdr_dbl_size_of_equivalent_value_range(struct hdr_dbl_histogram* h, double value);
-double hdr_dbl_lowest_equivalent_value(struct hdr_dbl_histogram* h, double value);
-double hdr_dbl_highest_equivalent_value(struct hdr_dbl_histogram* h, double value);
-double hdr_dbl_median_equivalent_value(struct hdr_dbl_histogram* h, double value);
-bool hdr_dbl_values_are_equivalent(struct hdr_dbl_histogram* h, double a, double b);
+double hdr_dbl_size_of_equivalent_value_range(const struct hdr_dbl_histogram* h, double value);
+double hdr_dbl_lowest_equivalent_value(const struct hdr_dbl_histogram* h, double value);
+double hdr_dbl_highest_equivalent_value(const struct hdr_dbl_histogram* h, double value);
+double hdr_dbl_median_equivalent_value(const struct hdr_dbl_histogram* h, double value);
+bool hdr_dbl_values_are_equivalent(const struct hdr_dbl_histogram* h, double a, double b);
 int64_t hdr_dbl_add_while_correcting_for_coordinated_omission(
         struct hdr_dbl_histogram** dest,
-        struct hdr_dbl_histogram* src,
+        const struct hdr_dbl_histogram* src,
         double expected_interval);
-double hdr_dbl_mean(struct hdr_dbl_histogram* h);
-double hdr_dbl_value_at_percentile(struct hdr_dbl_histogram* h, double percentile);
-double hdr_dbl_max(struct hdr_dbl_histogram* h);
-double hdr_dbl_min(struct hdr_dbl_histogram* h);
-double hdr_dbl_stddev(struct hdr_dbl_histogram *h);
+double hdr_dbl_mean(const struct hdr_dbl_histogram* h);
+double hdr_dbl_value_at_percentile(const struct hdr_dbl_histogram* h, double percentile);
+double hdr_dbl_max(const struct hdr_dbl_histogram* h);
+double hdr_dbl_min(const struct hdr_dbl_histogram* h);
+double hdr_dbl_stddev(const struct hdr_dbl_histogram *h);
 
 /**
  * Add the values from the addend histogram to the sum histogram.
  */
-int64_t hdr_dbl_add(struct hdr_dbl_histogram* sum, struct hdr_dbl_histogram* addend);
+int64_t hdr_dbl_add(struct hdr_dbl_histogram* sum, const struct hdr_dbl_histogram* addend);
 void hdr_dbl_reset(struct hdr_dbl_histogram* h);
 
 int64_t hdr_dbl_count_at_value(struct hdr_dbl_histogram* h, double value);
