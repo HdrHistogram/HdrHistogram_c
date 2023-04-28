f18d5aff301125aa3e1ef646c3aaa8c428c0245c
Michael Barker <mikeb01@gmail.com>
2023-04-28 14:03:37 +1200

    Update version.

--- include/hdr/hdr_histogram_version.h
+++ include/hdr/hdr_histogram_version.h
@@ -1,6 +1,7 @@
 /**
- * @file hdr_histogram_version.h
- * @brief Definitions for HdrHistogram's version number.
+ * hdr_histogram_version.h
+ * Written by Marco Ippolito, Michael Barker and released to the public domain,
+ * as explained at http://creativecommons.org/publicdomain/zero/1.0/
  */
 
 #ifndef HDR_HISTOGRAM_VERSION_H
